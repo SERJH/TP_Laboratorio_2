a7784106ca963581613ee3d75894d0bed1046b3e
Santiago Morán <santiagomoran11@gmail.com>
2017-10-27 01:10:47 -0300

    Add files via upload

---
+++ Santiago.Moran.2A.TP3/Clases Instanciables/Jornada.cs
@@ -0,0 +1,165 @@
+﻿using Clases_Abstractas;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Archivos;
+using Excepciones;
+
+namespace Clases_Instanciables {
+
+    public class Jornada {
+
+        private List<Alumno> _alumnos;
+        private Universidad.EClases _clase;
+        private Profesor _instructor;
+
+        public List<Alumno> Alumnos {
+
+            get { return this._alumnos; }
+            set { this._alumnos = value; }
+
+        }
+
+        public Universidad.EClases Clase {
+
+            get { return this._clase; }
+            set { this._clase = value; }
+
+        }
+
+        public Profesor Instructor {
+
+            get { return this._instructor; }
+            set { this._instructor = value; }
+
+        }
+
+        #region CONSTRUCTORES
+
+        private Jornada() {
+
+            _alumnos = new List<Alumno>();
+
+        }
+
+        public Jornada(Universidad.EClases clase, Profesor instructor) :this() {
+
+            this.Clase = clase;
+            this.Instructor = instructor;
+
+        }
+
+        #endregion CONSTRUCTORES
+
+        #region METODOS
+
+        public override string ToString() {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Clase: {0}\n", this.Clase);
+            sb.AppendFormat("\nInstructor:\n{0}\n", this.Instructor);
+            sb.AppendLine("Alumnos:\n");
+
+            foreach (Alumno i in this.Alumnos) {
+
+                sb.AppendLine(i.ToString());
+
+            }
+
+            sb.AppendLine("<--------------------------------------->");
+
+            return sb.ToString();
+
+        }
+
+        public static bool Guardar(Jornada jornada) {
+
+            try {
+
+                Texto texto = new Texto();
+
+                texto.Guardar("Jornada.txt", jornada.ToString());
+
+                return true;
+
+            } catch (Exception e) {
+
+                throw new ArchivosException(e);
+
+            }
+
+        }
+
+        public static string Leer() {
+
+            Texto texto = new Texto();
+
+            string salida;
+
+            texto.Leer("Jornada.txt", out salida);
+
+            return salida;
+
+        }
+
+        public 
+
+        #endregion METODOS
+
+        #region OPERADORES
+
+        static bool operator == (Jornada j, Alumno a) {
+
+            bool retorno = false;
+
+            if (a == j.Clase) {
+
+                retorno = true;
+
+            }
+
+            return retorno;
+
+        }
+
+        public static bool operator != (Jornada j, Alumno a) {
+
+            return !(j == a);
+
+        }
+
+        public static Jornada operator + (Jornada j, Alumno a) {
+
+            Jornada auxJornada = j;
+            bool esta = false;
+
+            foreach (Alumno i in auxJornada.Alumnos) {
+
+                if (a == i) {
+
+                    esta = true;
+                    break;
+
+                }
+
+            }
+
+            if (!esta) {
+
+                auxJornada.Alumnos.Add(a);
+
+            }
+
+            return auxJornada;
+
+        }
+
+        #endregion OPERADORES
+
+    }
+
+}
---
+++ Santiago.Moran.2A.TP3/Entidades/Universitario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas {
+
+    public abstract class Universitario : Persona {
+
+        private int _legajo;
+
+        #region CONSTRUCTORES
+
+        public Universitario() :base() {
+
+            this._legajo = 0;
+
+        }
+
+        public Universitario(int legajo, string nombre, string apellido, string dni, ENacionalidad nacionalidad) :base(nombre, apellido, dni, nacionalidad) {
+
+            this._legajo = legajo;
+
+        }
+
+        #endregion CONSTRUCTORES
+
+        #region METODOS
+
+        public override bool Equals(object obj) {
+
+            return ((this is Universitario && obj is Universitario) &&
+                   ((this._legajo == ((Universitario)obj)._legajo) || this.DNI == ((Universitario)obj).DNI));
+
+        }
+
+        protected virtual string MostrarDatos() {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(base.ToString());
+            sb.AppendFormat("Legajo: {0}\n", this._legajo);
+
+            return sb.ToString();
+
+        }
+
+        protected abstract string ParticiparEnClase();
+
+        #endregion METODOS
+
+        #region OPERADORES
+
+        public static bool operator == (Universitario pg1, Universitario pg2) {
+
+            return pg1.Equals(pg2);
+
+        }
+
+        public static bool operator != (Universitario pg1, Universitario pg2) {
+
+            return !(pg1 == pg2);
+
+        }
+
+        #endregion OPERADORES
+
+    }
+
+}
